dbe97241ed738b9e9c09e2f86a038b3b73f5bd5c
DankAvHi <thexcat86@gmail.com>
2022-11-12 14:59:44 +0300

    added navigation to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import "../styles/index.css";
+import Navigation from "./components/Navigation/Navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
      return (
           <html>
                <head />
-               <body>{children}</body>
+               <body>
+                    <Navigation />
+                    {children}
+               </body>
           </html>
      );
 }
