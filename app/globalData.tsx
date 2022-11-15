aa8f4f6ea385b642be7fa318174557eba89d9b2a
DankAvHi <thexcat86@gmail.com>
2022-11-15 10:36:07 +0300

    create file with global data; add arr of site nav

---
+++ app/globalData.tsx
@@ -0,0 +1,6 @@
+export const sitePages = [
+     { label: "home", linkText: "Главная", href: "/" },
+     { label: "gallery", linkText: "Галерея", href: "/gallery" },
+     { label: "aboutUs", linkText: "О нас", href: "/about-us" },
+     { label: "contacts", linkText: "Контакты", href: "/contacts" },
+];
