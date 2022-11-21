23c3622fabb1cd80451a4770525f89521bab0c02
DankAvHi <thexcat86@gmail.com>
2022-11-21 14:18:19 +0300

    change home page route

--- app/globalData.tsx
+++ app/globalData.tsx
@@ -1,5 +1,5 @@
 export const sitePages = [
-     { label: "home", linkText: "Главная", href: "/" },
+     { label: "home", linkText: "Главная", href: "/home" },
      { label: "gallery", linkText: "Галерея", href: "/gallery" },
      { label: "aboutUs", linkText: "О нас", href: "/about-us" },
      { label: "contacts", linkText: "Контакты", href: "/contacts" },
