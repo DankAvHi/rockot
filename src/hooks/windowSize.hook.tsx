2e2047860888b0d1fe80dde95ceb9a900783d478
DankAvHi <thexcat86@gmail.com>
2022-12-15 00:40:59 +0300

    [fix] chng useLayoutEffect - useEffect as next rec

--- src/hooks/windowSize.hook.tsx
+++ src/hooks/windowSize.hook.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useWindowSize() {
      const [size, setSize] = useState([0, 0]);
-     useLayoutEffect(() => {
+     useEffect(() => {
           function updateSize() {
                setSize([window.innerWidth, window.innerHeight]);
           }
