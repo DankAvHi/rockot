8c5069f206f8033d886526a8188807a313e836c1
DankAvHi <thexcat86@gmail.com>
2022-12-15 00:16:16 +0300

    [ref] throw any to trash; add unknown to err info and generic

--- src/utils/fetcher.util.ts
+++ src/utils/fetcher.util.ts
@@ -1,8 +1,8 @@
-const fetcher = async <JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> => {
+const fetcher = async <JSON = unknown>(input: RequestInfo, init?: RequestInit): Promise<JSON> => {
      const res = await fetch(input, init);
 
      if (!res.ok) {
-          const error: Error & { info: any; status: number } = {
+          const error: Error & { info: unknown; status: number } = {
                ...new Error("An error occurred while fetching the data."),
                info: await res.json(),
                status: res.status,
