60dac306f3c789f0f0087b7527f7571af86befa5
DankAvHi <thexcat86@gmail.com>
2022-12-23 14:40:33 +0300

    [add] added layout and styles for ChampionsSection

--- src/app/home/sections/ChampionsSection/ChampionsSection.tsx
+++ src/app/home/sections/ChampionsSection/ChampionsSection.tsx
@@ -5,6 +5,27 @@ import ChampionsSectionText from "./ChampionsSection.text";
 const ChampionsSection = () => {
      return (
           <section className={styles.ChampionsSection}>
+               <div className={styles.text}>
+                    <h2 className={styles.heading}>{ChampionsSectionText.heading}</h2>
+                    <p className={styles.paragraph}>
+                         {ChampionsSectionText.paragraph1.text1}
+                         <span className={`${textStyles.accientText} ${styles.logoText}`}>
+                              {ChampionsSectionText.paragraph1.logoText1}
+                         </span>
+                         {ChampionsSectionText.paragraph1.text2}
+                    </p>
+                    <p className={styles.paragraph}>{ChampionsSectionText.paragraph2}</p>
+                    <p className={styles.paragraph}>{ChampionsSectionText.paragraph3}</p>
+                    <ul className={styles.list}>
+                         {ChampionsSectionText.list1.map((item) => (
+                              <li className={styles.listItem} key={item}>
+                                   {item}
+                              </li>
+                         ))}
+                    </ul>
+               </div>
+
+               <div className={styles.imageWrapper}></div>
           </section>
      );
 };
