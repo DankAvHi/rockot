5158f49c905e291619aa80be15d53128ca4b8979
DankAvHi <thexcat86@gmail.com>
2022-12-23 14:37:44 +0300

    [ref; styles] some refactore

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,15 @@
 import globalStyles from "../../styles/global.module.css";
-import Header from "./components/Header/Header";
 import styles from "./home.module.css";
 import ChampionsSection from "./sections/ChampionsSection/ChampionsSection";
+import Header from "./sections/Header/Header";
 
 const page = () => {
      return (
           <>
                <Header />
+               <main className={`${styles.main} ${globalStyles.rootContainer}`}>
                     <ChampionsSection />
+               </main>
           </>
      );
 };
