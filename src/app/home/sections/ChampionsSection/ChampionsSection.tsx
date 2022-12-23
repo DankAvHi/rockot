9c66c9c8be5c9a3de0f9a75a29c51837f05092f6
DankAvHi <thexcat86@gmail.com>
2022-12-23 14:35:23 +0300

    [crt] created Champions Section files

---
+++ src/app/home/sections/ChampionsSection/ChampionsSection.tsx
@@ -0,0 +1,12 @@
+import textStyles from "../../../../styles/text.module.css";
+import styles from "./ChampionsSection.module.css";
+import ChampionsSectionText from "./ChampionsSection.text";
+
+const ChampionsSection = () => {
+     return (
+          <section className={styles.ChampionsSection}>
+          </section>
+     );
+};
+
+export default ChampionsSection;
