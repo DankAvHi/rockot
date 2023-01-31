32e11996104c8e68953905ea7a4d66e77e666121
DankAvHi <thexcat86@gmail.com>
2023-01-31 13:35:49 +0300

    [del ref form] deleted links list for production; format file with new config

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,40 +1,25 @@
 "use client";
 import { useState } from "react";
 import useScrollPosition from "../../../hooks/scrollPosition.hook";
-import { sitePages } from "../../globalData";
-import List from "./components/List/List";
 import Logo from "./components/Logo/Logo";
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
-  const scroll = useScrollPosition();
+    const scroll = useScrollPosition();
 
-  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+    const [isNavigationOpen, setIsNavigationOpen] = useState(false);
 
-  const toggleNavigationButtonOnClickHandler = () => {
-    setIsNavigationOpen((prev) => !prev);
-  };
+    const toggleNavigationButtonOnClickHandler = () => {
+        setIsNavigationOpen((prev) => !prev);
+    };
 
-  return (
-    <nav
-      className={`${styles.Navigation} ${
-        scroll > 10 && styles.Navigation_opacity
-      }`}
-    >
-      <div
-        className={`${styles.container} ${
-          isNavigationOpen && styles.container_open
-        }`}
-      >
-        <Logo onClick={toggleNavigationButtonOnClickHandler} />
-        <List
-          linkOnClick={toggleNavigationButtonOnClickHandler}
-          isNavigationOpen={isNavigationOpen}
-          sitePages={sitePages}
-        />
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={`${styles.Navigation} ${scroll > 10 && styles.Navigation_opacity}`}>
+            <div className={`${styles.container} ${isNavigationOpen && styles.container_open}`}>
+                <Logo onClick={toggleNavigationButtonOnClickHandler} />
+            </div>
+        </nav>
+    );
 };
 
 export default Navigation;
