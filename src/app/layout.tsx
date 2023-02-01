246fdc570fa2d524fe4c6e2e4b95dff3c737eb6e
DankAvHi <thexcat86@gmail.com>
2023-02-01 15:53:43 +0300

    [ref chng upd] added styled component registry global styles  and use client to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
-import "../styles/index.css";
+"use client";
+import GlobalStyles from "../styles/index.styles";
+import StyledComponentsRegistry from "../styles/registry";
 import Footer from "./components/Footer/Footer";
 import Navigation from "./components/Navigation/Navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="ru">
-            <head />
-            <body>
-                <Navigation />
-                {children}
-                <Footer />
-            </body>
-        </html>
+        <StyledComponentsRegistry>
+            <>
+                <GlobalStyles />
+                <html lang="ru">
+                    <head />
+                    <body>
+                        <Navigation />
+                        {children}
+                        <Footer />
+                    </body>
+                </html>
+            </>
+        </StyledComponentsRegistry>
     );
 }
