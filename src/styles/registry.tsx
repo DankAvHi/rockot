21e1c868f3b3ab46325bc8c7f8286850935a2c0f
DankAvHi <thexcat86@gmail.com>
2023-02-01 15:49:11 +0300

    [crt] create registry file for styled components

--- src/styles/registry.tsx
+++ src/styles/registry.tsx
@@ -4,26 +4,18 @@ import { useServerInsertedHTML } from "next/navigation";
 import React, { useState } from "react";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
-export default function StyledComponentsRegistry({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
-  useServerInsertedHTML(() => {
-    const styles = styledComponentsStyleSheet.getStyleElement();
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
-    styledComponentsStyleSheet.instance.clearTag();
-    return <>{styles}</>;
-  });
+    useServerInsertedHTML(() => {
+        const styles = styledComponentsStyleSheet.getStyleElement();
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        styledComponentsStyleSheet.instance.clearTag();
+        return <>{styles}</>;
+    });
 
-  if (typeof window !== "undefined") return <>{children}</>;
+    if (typeof window !== "undefined") return <>{children}</>;
 
-  return (
-    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      {children}
-    </StyleSheetManager>
-  );
+    return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
 }
