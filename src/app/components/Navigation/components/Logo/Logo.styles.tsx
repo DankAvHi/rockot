80ac45e2e68e077bed62619b43ec84066f0d5555
DankAvHi <thexcat86@gmail.com>
2023-02-08 13:51:57 +0300

    [chng] get changes from main

--- src/app/components/Navigation/components/Logo/Logo.styles.tsx
+++ src/app/components/Navigation/components/Logo/Logo.styles.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import styled from "styled-components";
+import colors from "../../../../../styles/colors.styles";
 import Button from "../../../UI/Buttons/Button/Button";
 
 export const StyledLogo = styled(Button)`
+    border-radius: 100%;
+
+    background: linear-gradient(${colors.rockotGradientTop} 60%, ${colors.rockotGradientBottom});
+
     width: 60px;
     height: 60px;
 
@@ -13,6 +18,8 @@ export const StyledLogo = styled(Button)`
 `;
 
 export const LogoImage = styled(Image)`
+    border-radius: 100%;
+
     width: 60px;
     height: 60px;
 
