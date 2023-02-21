1c8abc9240585880e799c8ce7a40575e6c4cf913
DankAvHi <thexcat86@gmail.com>
2023-02-21 11:56:59 +0300

    [crt] create index file for styles

---
+++ src/styles/index.tsx
@@ -0,0 +1,7 @@
+export * from "./colors.styles";
+export * from "./global.styles";
+export * from "./common.styles";
+export * from "./registry";
+export * from "./text.styles";
+export * from "./variables.styles";
+export * from "./fonts";
