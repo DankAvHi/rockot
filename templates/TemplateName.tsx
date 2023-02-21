92b0374cfc490e97ccc5be0a3d21aafadafe0cfe
DankAvHi <thexcat86@gmail.com>
2023-02-21 11:24:34 +0300

    [chng] update generate react cli template

--- templates/TemplateName.tsx
+++ templates/TemplateName.tsx
@@ -2,8 +2,6 @@ import { StyledTemplateName } from "./TemplateName.styles";
 
 type TemplateNameProps = object;
 
-const TemplateName = (props: TemplateNameProps) => {
+export const TemplateName = (props: TemplateNameProps) => {
     return <StyledTemplateName></StyledTemplateName>;
 };
-
-export default TemplateName;
