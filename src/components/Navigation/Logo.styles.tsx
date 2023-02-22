165e73e423fd3584c15179a47c32526b81dc3179
DankAvHi <thexcat86@gmail.com>
2023-02-22 16:46:18 +0300

    [fix] fix svg sizes

--- src/components/Navigation/Logo.styles.tsx
+++ src/components/Navigation/Logo.styles.tsx
@@ -7,16 +7,22 @@ export const LogoStyles = {
     Logo: styled(Button)`
         border-radius: 100%;
 
+        padding: 2px;
+
         background: linear-gradient(${colors.rockotGradientTop} 60%, ${colors.rockotGradientBottom});
 
         width: 60px;
         height: 60px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
     `,
 
     LogoImage: styled(LogoMin)`
         border-radius: 100%;
 
-        width: 60px;
-        height: 60px;
+        width: 100%;
+        height: 100%;
     `,
 };
