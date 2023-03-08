cdb7f6cb3e2cfd05af1d0786f64ada2aa923ca50
DankAvHi <thexcat86@gmail.com>
2023-03-08 22:08:36 +0300

    [add] add fotter component root el styles

--- src/components/Footer/Footer.styles.tsx
+++ src/components/Footer/Footer.styles.tsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
+import { colors } from "../../styles";
 
 export const FooterStyles = {
-    Footer: styled.footer``,
+    Footer: styled.footer`
+        background: ${colors.black00};
+
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+    `,
 };
