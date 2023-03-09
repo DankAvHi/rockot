6c0feb0f84cf7c831b38a9a131872ec6720e8f34
DankAvHi <thexcat86@gmail.com>
2023-03-09 23:03:25 +0300

    [add] logo with site link into footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
 import { FooterStyles as S } from "./Footer.styles";
+import { FooterText } from "./Footer.text";
 
 export const Footer = () => {
     return (
         <S.Footer>
             <S.Container>
                 <S.MediaList>
+                    <S.MediaItem>
+                        <S.MediaLink type="link" href={"/"}>
+                            <S.LogoText>{FooterText.LogoText}</S.LogoText>
+                        </S.MediaLink>
+                    </S.MediaItem>
                 </S.MediaList>
             </S.Container>
         </S.Footer>
