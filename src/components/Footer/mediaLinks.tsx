7a6cc02e2947b572f216bf3dab9c85e4587c88e2
DankAvHi <thexcat86@gmail.com>
2023-03-09 22:52:47 +0300

    [crt] media links data file

---
+++ src/components/Footer/mediaLinks.tsx
@@ -0,0 +1,7 @@
+import { Instagram, Telegram, VK } from "@assets";
+
+export const mediaLinks = [
+    { name: "Instagram", href: "#", icon: Instagram },
+    { name: "Telegram", href: "#", icon: Telegram },
+    { name: "VK", href: "#", icon: VK },
+];
