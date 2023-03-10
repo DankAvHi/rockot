e5ac47cd07b893716fbfe1991541399d5e6d066c
DankAvHi <thexcat86@gmail.com>
2023-03-10 12:37:44 +0300

    [fix] add hrefs to footer media links

--- src/components/Footer/mediaLinks.tsx
+++ src/components/Footer/mediaLinks.tsx
@@ -1,7 +1,7 @@
 import { Instagram, Telegram, VK } from "@assets";
 
 export const mediaLinks = [
-    { name: "Instagram", href: "#", icon: Instagram },
-    { name: "Telegram", href: "#", icon: Telegram },
-    { name: "VK", href: "#", icon: VK },
+    { name: "Instagram", href: "https://www.instagram.com/rockot_krasnodar/", icon: Instagram },
+    { name: "Telegram", href: "https://t.me/rockot_krasnodar", icon: Telegram },
+    { name: "VK", href: "https://vk.com/rockot_krasnodar", icon: VK },
 ];
